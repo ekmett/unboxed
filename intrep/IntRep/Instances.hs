dcb85c9b5c0a6ee954353820f824da61bf5789fb
Edward Kmett <ekmett@gmail.com>
2021-02-19 01:08:06 -0800

    isTrue#

--- intrep/IntRep/Instances.hs
+++ intrep/IntRep/Instances.hs
@@ -8,6 +8,7 @@ module IntRep.Instances where
 
 import GHC.Prim
 import GHC.Integer
+import GHC.Types
 import IntRep.Rep
 import IntRep.Classes
 import IntRep.Syntax
@@ -17,12 +18,12 @@ instance Num Int# where
   (-) = (-#)
   (*) = (*#)
   negate = negateInt#
-  abs n 
-    | n >=# 0 = n 
+  abs n
+    | isTrue# (n >=# 0) = n
     | otherwise = negate n
   signum n
-    | n <# 0 = negate 1
-    | n ==# 0 = 0
+    | isTrue# (n <# 0) = negate 1
+    | isTrue# (n ==# 0) = 0
     | otherwise = 1
   fromInteger = integerToInt
   {-# INLINE fromInteger #-}
