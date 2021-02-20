ee00c5f3c403e36adad5f47ada8e477d23db8f8f
Edward Kmett <ekmett@gmail.com>
2021-02-19 18:52:45 -0800

    Unlifted.Prelude works as a better name for the top

--- src/Unlifted/Prelude.hs
+++ src/Unlifted/Prelude.hs
@@ -1,11 +1,23 @@
 -- eventually provide a Prelude alternative that is overloaded to allow unlifted operations where possible
 
-module Unlifted 
+module Unlifted.Prelude
   ( module Unlifted.Class
   , module Unlifted.Syntax
   , module Unlifted.Combinators
+  , Bool(..)
+  , (&&), (||), not
+  , Ordering(..)
+  , undefined
+  , error
   ) where
 
 import Unlifted.Class
 import Unlifted.Syntax
 import Unlifted.Combinators
+import Prelude
+  ( Bool(..)
+  , (&&), (||), not
+  , Ordering(..)
+  , undefined
+  , error
+  ) 
