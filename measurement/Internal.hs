e478e801edb356f6442e10016529ccce752244aa
Edward Kmett <ekmett@gmail.com>
2021-03-10 01:29:12 -0800

    fix benchmarking, now we don't look so good

---
+++ measurement/Internal.hs
@@ -0,0 +1,38 @@
+{-# Language MagicHash #-}
+{-# Language RankNTypes #-}
+{-# Language DataKinds #-}
+{-# Language PolyKinds #-}
+{-# OPTIONS_GHC -O2 -fno-prof-auto #-}
+-- Make the function applications in nf' and whnf' strict (avoiding allocation)
+-- and avoid floating out the computations.
+{-# OPTIONS_GHC -fno-full-laziness #-}
+module Internal where
+
+import Criterion.Measurement.Types
+import GHC.Types
+import Data.Int
+
+whnf'N#
+  :: forall (a :: TYPE ('TupleRep '[ 'WordRep, 'UnliftedRep ])) b.
+     (a -> b) -> a -> (Int64 -> IO ())
+whnf'N# f x = go where
+  go n
+    | n <= 0    = pure ()
+    | otherwise = f x `seq` go (n-1)
+{-# NOINLINE whnf'N# #-}
+
+whnf'B# :: forall (a :: TYPE ('SumRep '[ 'IntRep, 'LiftedRep ])) b.
+  (a -> b) -> a -> (Int64 -> IO ())
+whnf'B# f x = go where
+  go n
+    | n <= 0    = pure ()
+    | otherwise = f x `seq` go (n-1)
+{-# NOINLINE whnf'B# #-}
+
+whnf'L :: forall a b.
+  (a -> b) -> a -> (Int64 -> IO ())
+whnf'L f x = go where
+  go n
+    | n <= 0    = pure ()
+    | otherwise = f x `seq` go (n-1)
+{-# NOINLINE whnf'L #-}
--- measurement/Whnf.hs
+++ measurement/Whnf.hs
@@ -7,41 +7,17 @@ module Whnf where
 import Criterion.Measurement.Types
 import GHC.Types
 import Data.Int
-
-whnf'N#
-  :: forall (a :: TYPE ('TupleRep '[ 'WordRep, 'UnliftedRep ])) b.
-     (a -> b) -> a -> (Int64 -> IO ())
-whnf'N# f x = go where
-  go n
-    | n <= 0    = pure ()
-    | otherwise = f x `seq` go (n-1)
-{-# NOINLINE whnf'N# #-}
+import Internal
 
 whnfN#
   :: forall (a :: TYPE ('TupleRep '[ 'WordRep, 'UnliftedRep ])) b.
      (a -> b) -> a -> Benchmarkable
 whnfN# f x = toBenchmarkable (whnf'N# f x)
 
-whnf'B# :: forall (a :: TYPE ('SumRep '[ 'IntRep, 'LiftedRep ])) b.
-  (a -> b) -> a -> (Int64 -> IO ())
-whnf'B# f x = go where
-  go n
-    | n <= 0    = pure ()
-    | otherwise = f x `seq` go (n-1)
-{-# NOINLINE whnf'B# #-}
-
 whnfB#
   :: forall (a :: TYPE ('SumRep '[ 'IntRep, 'LiftedRep ])) b.
      (a -> b) -> a -> Benchmarkable
 whnfB# f x = toBenchmarkable (whnf'B# f x)
-
-whnf'L :: forall a b.
-  (a -> b) -> a -> (Int64 -> IO ())
-whnf'L f x = go where
-  go n
-    | n <= 0    = pure ()
-    | otherwise = f x `seq` go (n-1)
-{-# NOINLINE whnf'L #-}
 
 whnfL
   :: forall a b.
