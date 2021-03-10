e4c6ca80fb8946b1abfe595ba1c36587a33931db
Edward Kmett <ekmett@gmail.com>
2021-03-10 01:55:22 -0800

    cargo cult https://gitlab.haskell.org/ghc/ghc/-/commit/5cfe4d6bbe12a4c7d560fc19e76d781907bf7bda

--- src/Unboxed/Internal/Natural.hs
+++ src/Unboxed/Internal/Natural.hs
@@ -29,7 +29,8 @@ import Unboxed.Rep.Word ()
 
 -- #define CONSTANT_FOLDED INLINE
 -- #define CONSTANT_FOLDED NOINLINE
-#define CONSTANT_FOLDED INLINE[0]
+-- #define CONSTANT_FOLDED INLINE[0]
+#define CONSTANT_FOLDED NOINLINE[0]
 
 newtype Natural# = Natural# (# Word#, BigNat# #)
 
